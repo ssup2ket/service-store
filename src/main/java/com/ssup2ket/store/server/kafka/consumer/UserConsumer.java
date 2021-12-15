846bd366fb5d730a934d7e13edd3fc18d760915a
ssup2 <supsup5642@gmail.com>
2021-12-15 14:33:45 +0900

    Fix user consumer

--- src/main/java/com/ssup2ket/store/server/kafka/consumer/UserConsumer.java
+++ src/main/java/com/ssup2ket/store/server/kafka/consumer/UserConsumer.java
@@ -3,6 +3,7 @@ package com.ssup2ket.store.server.kafka.consumer;
 import brave.Tracer;
 import brave.propagation.TraceContextOrSamplingFlags;
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.ssup2ket.store.domain.model.Inbox;
 import com.ssup2ket.store.domain.service.ManagementService;
 import com.ssup2ket.store.pkg.tracing.SpanContext;
@@ -10,7 +11,9 @@ import com.ssup2ket.store.server.kafka.message.DebezOutbox;
 import java.util.UUID;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.support.Acknowledgment;
 import org.springframework.messaging.handler.annotation.Header;
+import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -25,28 +28,39 @@ public class UserConsumer {
       topics = "#{${spring.kafka.topic.prefix}}-ssup2ket-auth-outbox-User",
       groupId = "#{${spring.kafka.groupid.prefix}}-ssup2ket-store-user")
   public void consume(
-      @Header("id") String msgId, @Header("spanContext") String spanContextJson, DebezOutbox msg) {
-    if (msg.getEventType() == "UserDelete") {
-      // Convert debezium outbox message to inbox
-      Inbox inbox =
-          new Inbox(
-              UUID.fromString(msgId),
-              AGGREGATE_TYPE,
-              EVENT_TYPE_DELETE,
-              msg.getPayload(),
-              spanContextJson);
+      @Header("id") String msgId,
+      @Header("spanContext") String spanContextJson,
+      @Payload String msg,
+      Acknowledgment ack) {
 
-      // Set span context
-      try {
+    try {
+      // Get outbox object
+      ObjectMapper objectMapper = new ObjectMapper();
+      DebezOutbox outbox = objectMapper.readValue(msg, DebezOutbox.class);
+
+      if (outbox.getEventType() == "UserDelete") {
+        // Convert debezium outbox message to inbox
+        Inbox inbox =
+            new Inbox(
+                UUID.fromString(msgId),
+                AGGREGATE_TYPE,
+                EVENT_TYPE_DELETE,
+                outbox.getPayload(),
+                spanContextJson);
+
+        // Set span context
         TraceContextOrSamplingFlags spanContext =
             SpanContext.GetSpanContextFromJson(spanContextJson);
         tracer.nextSpan(spanContext);
-      } catch (JsonProcessingException e) {
-        throw new RuntimeException(e);
-      }
 
-      // Call service
-      managementService.deleteStoreProudctByRemovedUser(inbox);
+        // Call service
+        managementService.deleteStoreProudctByRemovedUser(inbox);
+
+        // ACK
+
+      }
+    } catch (JsonProcessingException e) {
+      throw new RuntimeException(e);
     }
   }
 }
--- src/main/java/com/ssup2ket/store/server/kafka/message/DebezOutbox.java
+++ src/main/java/com/ssup2ket/store/server/kafka/message/DebezOutbox.java
@@ -1,10 +1,12 @@
 package com.ssup2ket.store.server.kafka.message;
 
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
+@NoArgsConstructor
 public class DebezOutbox {
   private String schema;
   private String payload;
